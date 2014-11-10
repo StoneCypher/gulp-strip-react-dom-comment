214116bf0df697e102015f0fdfd247d6e9cf1864
John Haugeland <stonecypher@gmail.com>
2014-11-09 20:34:47 -0800

    Added and passing sc-eslint

--- stripdom.js
+++ stripdom.js
@@ -1,18 +1,21 @@
-var es    = require('event-stream'),
-    gutil = require('gulp-util');
+
+/* jshint node: true */
+/* eslint-env node, browser */
+
+var es    = require("event-stream");
 
 
 
 module.exports = function() {
 
-    'use strict';
+    "use strict";
 
     return es.map( function (file, callback) {
 
-        file.contents = new Buffer( 
-            file.contents.toString().replace('/** @jsx React.DOM */', '')
+        file.contents = new Buffer(
+            file.contents.toString().replace("/** @jsx React.DOM */", "")
         );
-        
+
         callback(null, file);
 
     } );
